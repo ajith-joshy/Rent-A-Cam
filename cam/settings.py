1931c1901b2baf299a2cb6243f07d6359f66ee6c
ajith <ajithmechery11@gmail.com>
2026-03-08 15:27:37 +0530

    css styling changes

--- cam/settings.py
+++ cam/settings.py
@@ -92,7 +92,9 @@ STATICFILES_DIRS=[
     BASE_DIR/"static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL='/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 import cloudinary
 cloudinary.config(
