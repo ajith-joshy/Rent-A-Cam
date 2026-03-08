42d799cd340f505cdd427b75fab4b436ea099492
ajith <ajithmechery11@gmail.com>
2026-03-08 15:05:10 +0530

    switched image storage to cloudinary

--- cam/settings.py
+++ cam/settings.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
 from dotenv import load_dotenv
-load_dotenv()
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -87,17 +87,21 @@ TEMPLATES = [
         },
     },
 ]
-
-STATICFILES_DIRS=[BASE_DIR/"static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR/"static",
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-# MEDIA_URL='/media/'
-# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
-    'API_KEY': os.getenv("CLOUDINARY_API_KEY"),
-    'API_SECRET': os.getenv("CLOUDINARY_API_SECRET"),
-}
+import cloudinary
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 WSGI_APPLICATION = 'cam.wsgi.application'
@@ -146,8 +150,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- superuser/models.py
+++ superuser/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=30)
     description=models.TextField()
-    image=models.ImageField(upload_to='categories')
+    image=CloudinaryField('image')
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     name=models.CharField(max_length=30)
     description = models.TextField()
-    image=models.ImageField(upload_to="products")
+    image=CloudinaryField('image')
     price=models.IntegerField()
     stock=models.IntegerField()
     available=models.BooleanField(default=True)
