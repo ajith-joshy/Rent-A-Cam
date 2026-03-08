6f96627cd365db45c964585a629a605804917028
ajith <ajithmechery11@gmail.com>
2026-03-08 15:28:28 +0530

    css styling changes

--- cam/urls.py
+++ cam/urls.py
@@ -28,4 +28,3 @@ urlpatterns = [
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
