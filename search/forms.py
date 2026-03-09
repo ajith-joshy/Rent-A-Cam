7e2ce516abc4d4c9b5977c12e093a3724801d8e1
ajith <ajithmechery11@gmail.com>
2026-03-09 23:50:50 +0530

    review error fixed

--- search/forms.py
+++ search/forms.py
@@ -2,11 +2,15 @@ from .models import Review
 from django import forms
 
 class Reviewform(forms.ModelForm):
-    rating = forms.IntegerField(required=True)
-    comment = forms.CharField(required=True, widget=forms.Textarea)
+    rating = forms.IntegerField(
+        required=True,
+        error_messages={
+            'required': 'Please select a rating'
+        }
+    )
     class Meta:
-        model=Review
-        fields=['rating','comment']
+        model = Review
+        fields = ['rating','comment']
         widgets = {
             'rating': forms.NumberInput(attrs={'class': 'form-control'}),
             'comment': forms.Textarea(attrs={'class': 'form-control'})
